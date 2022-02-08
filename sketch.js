18d429dffa63c892b57ff8561f5f28c25a82d386
gauravkmjaiswal <56041706+gauravkmjaiswal@users.noreply.github.com>
2022-02-08 22:31:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+let capture;
+let posenet;
+let noseX,noseY;
+let reyeX,reyeY;
+let leyeX,leyeY;
+let singlePose,skeleton;
+let actor_img;
+let specs,smoke;
+//////
+function setup() {
+    // createCanvas(800, 500);
+    createCanvas(700, 480);
+    capture = createCapture(VIDEO)
+    capture.hide();
+    posenet = ml5.poseNet(capture, modelLoaded);
+    posenet.on('pose',receivedPoses);
+
+}
+
+function receivedPoses(poses){
+    console.log(poses);
+    if(poses.length > 0){
+        singlePose = poses[0].pose;
+        skeleton = poses[0].skeleton;
+        document.querySelector(".subHeader").textContent=" Person Detected"
+
+    }
+    else
+    {
+            document.querySelector(".subHeader").textContent=" Clear Area"
+    }
+}
+
+function modelLoaded() {
+    console.log('Model has loaded');
+}
+
+function draw() {
+    image(capture, 0, 0);
+    fill(255,0,0);
+
+    if(singlePose){
+        for(let i=0; i<singlePose.keypoints.length; i++){
+            ellipse(singlePose.keypoints[i].position.x, singlePose.keypoints[i].position.y,20);
+        }
+        stroke(255,255,255);
+        strokeWeight(5);
+        for(let j=0; j<skeleton.length; j++){
+            line(skeleton[j][0].position.x, skeleton[j][0].position.y, skeleton[j][1].position.x, skeleton[j][1].position.y)
+        }
+    }
+    else
+    {
+        document.querySelector(".subHeader").textContent=" Clear Area"
+    }
+
+    
+
+}
